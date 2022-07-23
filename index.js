bade60d015154cc867b14e9f3e9dcb8bfb0bb5b8
jahidshawon19 <jahidshawon1730@gmail.com>
2022-07-23 08:23:13 +0600

    set up node server and connect with mongodb

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId
+require('dotenv').config()
+const app = express() 
+const port = 8000 
+
+
+app.use(cors())
+app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qypzm.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+async function run() {
+    try {
+      await client.connect();
+  
+
+    } finally {
+        // await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+
+app.get('/', (req, res)=>{
+
+    res.send('Artisan Trip server running ...')
+})  
+
+app.listen(port, ()=>{
+    console.log(`Server running at ${port}`)
+})
+
+
