ad65302e14be91ee6e131f94bdcfbbef6fbc2a80
jahidshawon19 <jahidshawon1730@gmail.com>
2022-09-19 23:49:32 +0600

    Alhamdulliah

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const cors = require('cors')
 const ObjectId = require('mongodb').ObjectId
 require('dotenv').config()
 const app = express() 
+const SSLCommerzPayment = require('sslcommerz')
 const port = process.env.PORT || 5000; 
 
 
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qypzm.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -150,6 +152,91 @@ app.get('/', (req, res)=>{
 
     res.send('<h1>Artisan Trip server running ...</h1>')
 })  
+
+
+
+
+
+// ########################  PAYMENT INITIALIZATION START ##################### //
+
+
+app.post('/init', (req, res) => {
+ 
+  const data = {
+      total_amount: req.body.total_amount,
+      currency: 'BDT',
+      tran_id: 'REF123',
+      success_url: 'http://localhost:5000/success',
+      fail_url: 'http://localhost:5000/fail',
+      cancel_url: 'http://localhost:5000/cancel',
+      ipn_url: 'http://localhost:5000/ipn',
+      shipping_method: 'Courier',
+      product_name: req.body.product_name,
+      product_category: 'Electronic',
+      product_profile: 'general',
+      cus_name: req.body.cus_name,
+      cus_email: req.body.cus_email,
+      cus_add1: 'Dhaka',
+      cus_add2: 'Dhaka',
+      cus_city: 'Dhaka',
+      cus_state: 'Dhaka',
+      cus_postcode: '1000',
+      cus_country: 'Bangladesh',
+      cus_phone: '01711111111',
+      cus_fax: '01711111111',
+      ship_name: 'Customer Name',
+      ship_add1: 'Dhaka',
+      ship_add2: 'Dhaka',
+      ship_city: 'Dhaka',
+      ship_state: 'Dhaka',
+      ship_postcode: 1000,
+      ship_country: 'Bangladesh',
+      multi_card_name: 'mastercard',
+      value_a: 'ref001_A',
+      value_b: 'ref002_B',
+      value_c: 'ref003_C',
+      value_d: 'ref004_D'
+  };
+
+  const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASSWORD,false) //true for live default false for sandbox
+  sslcommer.init(data).then(data => {
+      //process the response that got from sslcommerz 
+      //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+   
+     
+      if(data.GatewayPageURL){
+        res.json(data.GatewayPageURL)
+      }else{
+        return res.status(400).json({
+          message:'Payment Session Failed'
+        })
+      }
+  });
+
+
+})
+
+
+app.post('/success', async(req, res) =>{
+ 
+  res.status(200).redirect('http://localhost:3000/success')
+})
+
+app.post('/fail', async (req, res) =>{
+  res.status(200).redirect('http://localhost:3000')
+})
+
+
+app.post('/cancel', async (req, res) =>{
+  res.status(200).redirect('http://localhost:3000')
+})
+
+
+
+// ########################  PAYMENT INITIALIZATION END ##################### //
+
+
+
 
 app.listen(port, ()=>{
     console.log(`Server running at ${port}`)
